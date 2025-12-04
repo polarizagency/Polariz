dd4a396273e36717c443f31424e50d0ead2115b1
remiedmond <remi.emond6230@gmail.com>
2025-12-04 10:45:28 +0100

    Setup React + Tailwind v4 fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+  <>
+        <h1 className="text-4xl font-extrabold bg-gradient-to-r mb-4">
+          Polariz Agency
+        </h1>
+        <button className="px-6 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-full font-semibold ">
+          Test
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </> 
   )
 }
-
-export default App
